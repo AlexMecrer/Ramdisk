8abc7aa92044b0f6adcf16a7293977f460edbe14
AlexMecrer <2927518585@qq.com>
2018-05-18 17:16:53 +0800

    Add Create Queue

--- RamDisk.c
+++ RamDisk.c
@@ -14,14 +14,30 @@ EvtDiskDriverDeviceAdd(
 {
 	NTSTATUS status = STATUS_SUCCESS;
 	UNICODE_STRING DeviceName = RTL_CONSTANT_STRING(DEVICENAME);
-	WDFDEVICE* Device;
+	WDFDEVICE Device;
 	WDF_OBJECT_ATTRIBUTES Attr;
+	WDF_IO_QUEUE_CONFIG Config;
+	WDFQUEUE Queue;
 	WDF_OBJECT_ATTRIBUTES_INIT(&Attr);
 	status=WdfDeviceInitAssignName(DeviceInit,&DeviceName);
 	WdfDeviceInitSetIoType(DeviceInit,WdfDeviceIoDirect);
 	WdfDeviceInitSetExclusive(DeviceInit,FALSE);
 	WdfDeviceInitSetDeviceType(DeviceInit,FILE_DEVICE_DISK);
 	WDF_OBJECT_ATTRIBUTES_INIT_CONTEXT_TYPE(&Attr,DISK_EXTENSION);//设置设备上下文
-	status = WdfDeviceCreate(&DeviceInit,&Attr,Device);
+	status = WdfDeviceCreate(&DeviceInit,&Attr,&Device);
+	if (!NT_SUCCESS(status))
+	{
+		KdPrint(("Disk Device Create Fail\r\n"));
+	}
+	WDF_IO_QUEUE_CONFIG_INIT(&Config,WdfIoQueueDispatchSequential);
+	Config.EvtIoRead = RamDiskEvtIoRead;
+	Config.EvtIoWrite = RamDiskEvtIoWrite;
+	Config.EvtIoDeviceControl = RamDiskEvtIoDeviceControl;
+	WDF_OBJECT_ATTRIBUTES_INIT_CONTEXT_TYPE(&Attr, QUEUE_EXTENSION);
+	status=WdfIoQueueCreate(Device,&Config,&Attr,&Queue);
+	if (!NT_SUCCESS(status))
+	{
+		KdPrint(("Queue Create Fail\r\n"));
+	}
 	return status;
 }
