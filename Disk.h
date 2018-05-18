50b349dce54f841ff24cbe0c510cf547d4fbccba
AlexMecrer <2927518585@qq.com>
2018-05-18 17:28:09 +0800

    Add Disk Format

---
+++ Disk.h
@@ -0,0 +1,53 @@
+#pragma once
+#include<wdm.h>
+#pragma pack(1)
+typedef struct  _BOOT_SECTOR
+{
+	UCHAR       bsJump[3];          // x86 jmp instruction, checked by FS
+	CCHAR       bsOemName[8];       // OEM name of formatter
+	USHORT      bsBytesPerSec;      // Bytes per Sector
+	UCHAR       bsSecPerClus;       // Sectors per Cluster
+	USHORT      bsResSectors;       // Reserved Sectors
+	UCHAR       bsFATs;             // Number of FATs - we always use 1
+	USHORT      bsRootDirEnts;      // Number of Root Dir Entries
+	USHORT      bsSectors;          // Number of Sectors
+	UCHAR       bsMedia;            // Media type - we use RAMDISK_MEDIA_TYPE
+	USHORT      bsFATsecs;          // Number of FAT sectors
+	USHORT      bsSecPerTrack;      // Sectors per Track - we use 32
+	USHORT      bsHeads;            // Number of Heads - we use 2
+	ULONG       bsHiddenSecs;       // Hidden Sectors - we set to 0
+	ULONG       bsHugeSectors;      // Number of Sectors if > 32 MB size
+	UCHAR       bsDriveNumber;      // Drive Number - not used
+	UCHAR       bsReserved1;        // Reserved
+	UCHAR       bsBootSignature;    // New Format Boot Signature - 0x29
+	ULONG       bsVolumeID;         // VolumeID - set to 0x12345678
+	CCHAR       bsLabel[11];        // Label - set to RamDisk
+	CCHAR       bsFileSystemType[8];// File System Type - FAT12 or FAT16
+	CCHAR       bsReserved2[448];   // Reserved
+	UCHAR       bsSig2[2];          // Originial Boot Signature - 0x55, 0xAA
+}   BOOT_SECTOR, *PBOOT_SECTOR;
+
+typedef struct  _DIR_ENTRY
+{
+	UCHAR       deName[8];          // File Name
+	UCHAR       deExtension[3];     // File Extension
+	UCHAR       deAttributes;       // File Attributes
+	UCHAR       deReserved;         // Reserved
+	USHORT      deTime;             // File Time
+	USHORT      deDate;             // File Date
+	USHORT      deStartCluster;     // First Cluster of file
+	ULONG       deFileSize;         // File Length
+}   DIR_ENTRY, *PDIR_ENTRY;
+
+#pragma pack()
+
+//
+// Directory Entry Attributes
+//
+
+#define DIR_ATTR_READONLY   0x01
+#define DIR_ATTR_HIDDEN     0x02
+#define DIR_ATTR_SYSTEM     0x04
+#define DIR_ATTR_VOLUME     0x08
+#define DIR_ATTR_DIRECTORY  0x10
+#define DIR_ATTR_ARCHIVE    0x20
--- RamDisk.h
+++ RamDisk.h
@@ -3,11 +3,12 @@
 #include<ntdddisk.h>
 #include<ntstrsafe.h>
 #include<wdf.h>
+#include"Disk.h"
+
 
 #define DEVICENAME L"\\Devices\\RamDisk"
 
 
-//下面的宏登记一个获取函数并开辟上下文，每一个作为上下文的结构体都必须使用这个宏
 typedef struct _DISK_EX{
 	USHORT cbSize;
 }DISK_EXTENSION,*PDISK_EXTENSION;
@@ -16,6 +17,8 @@ typedef struct Queue_Ex {
 	USHORT cbSize;
 }QUEUE_EXTENSION,*PQUEUE_EXTENSION;
 
+
+//下面的宏登记一个获取函数并开辟上下文，每一个作为上下文的结构体都必须使用这个宏
 WDF_DECLARE_CONTEXT_TYPE_WITH_NAME(DISK_EXTENSION,GetDiskExtension)
 WDF_DECLARE_CONTEXT_TYPE_WITH_NAME(QUEUE_EXTENSION,GetQueueExtension)
 
