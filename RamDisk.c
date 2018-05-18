7e1429043d4f6d43c8f6116a5dbcb5776e8b9fef
AlexMecrer <2927518585@qq.com>
2018-05-18 16:52:42 +0800

    Create a Device with Wdf

--- RamDisk.c
+++ RamDisk.c
@@ -5,17 +5,23 @@ EvtDiskDriverDeviceAdd(
 	WDFDRIVER Driver,
 	PWDFDEVICE_INIT DeviceInit
 )
+/*
+函数名：EvtDiskDriverDeviceAdd
+描述：使用WdfDeviceInit*函数设置将要创建设备的属性（操作DeviceInit），使用Attr
+设置设备的上下文调用WdfDeviceCreate创建目标设备
+返回值：依赖WdfDeviceCreate
+*/
 {
 	NTSTATUS status = STATUS_SUCCESS;
 	UNICODE_STRING DeviceName = RTL_CONSTANT_STRING(DEVICENAME);
 	WDFDEVICE* Device;
 	WDF_OBJECT_ATTRIBUTES Attr;
 	WDF_OBJECT_ATTRIBUTES_INIT(&Attr);
-	WDF_OBJECT_ATTRIBUTES_INIT_CONTEXT_TYPE(&Attr,DISK_EX);
 	status=WdfDeviceInitAssignName(DeviceInit,&DeviceName);
 	WdfDeviceInitSetIoType(DeviceInit,WdfDeviceIoDirect);
 	WdfDeviceInitSetExclusive(DeviceInit,FALSE);
 	WdfDeviceInitSetDeviceType(DeviceInit,FILE_DEVICE_DISK);
-	status = WdfDeviceCreate(&DeviceInit,WDF_NO_OBJECT_ATTRIBUTES,Device);
+	WDF_OBJECT_ATTRIBUTES_INIT_CONTEXT_TYPE(&Attr,DISK_EXTENSION);//设置设备上下文
+	status = WdfDeviceCreate(&DeviceInit,&Attr,Device);
 	return status;
 }
--- RamDisk.h
+++ RamDisk.h
@@ -7,12 +7,12 @@
 #define DEVICENAME L"\\Devices\\RamDisk"
 
 
-
-typedef struct {
+//下面的宏登记一个获取函数并开辟上下文，每一个作为上下文的结构体都必须使用这个宏
+typedef struct _DISK_EX{
 	USHORT cbSize;
-}DISK_EX,*PDISK_EX;
+}DISK_EXTENSION,*PDISK_EXTENSION;
 
-
+WDF_DECLARE_CONTEXT_TYPE_WITH_NAME(DISK_EXTENSION,GetDiskExtension)
 
 
 NTSTATUS
