f1d56afe488bc46d685182e581f0e03cc5a2cd3d
AlexMecrer <2927518585@qq.com>
2018-05-18 16:51:28 +0800

    Nothing change

--- Main.c
+++ Main.c
@@ -5,8 +5,15 @@ DriverEntry(
 	IN PDRIVER_OBJECT DriverObject,
 	IN PUNICODE_STRING RegistryPath
 )
+/*
+函数名：DriverEntry
+功能描述：设置驱动的DriverAdd函数
+返回值；依赖于WdfDriverCreate函数
+*/
 {
 	WDF_DRIVER_CONFIG Config;
 	WDF_DRIVER_CONFIG_INIT(&Config,EvtDiskDriverDeviceAdd);
 	return WdfDriverCreate(DriverObject,RegistryPath, WDF_NO_OBJECT_ATTRIBUTES,&Config,WDF_NO_HANDLE);
 }
+
+
